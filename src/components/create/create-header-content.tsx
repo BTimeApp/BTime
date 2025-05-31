e08459fa93299cb976842f178f61344b7144d69c
Heidi Chan <heidi.chan@berkeley.edu>
2025-05-31 11:42:41 -0700

    working on create page, commit this for now so it's not lost while figuring out things

---
+++ src/components/create/create-header-content.tsx
@@ -0,0 +1,11 @@
+export default function CreateRoomHeader({isStarted}: {isStarted?: boolean}) { 
+
+    //const exampleScramble = "Scramble Placeholder";
+    return (
+        <>
+            <h1 className="grow font-bold text-center text-2xl">
+                Create Room
+            </h1>
+        </>
+    );
+}
