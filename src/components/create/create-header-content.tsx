697b159bf71f71bb5ddf8c563bb82f6c83b14245
Heidi Chan <heidi.chan@berkeley.edu>
2025-06-04 11:19:59 -0700

    Revert "Bare bones create page, still need to clean up style and connecting everything together"
    This reverts commit d7e4f7afe020e0b6863bf36696d678a31d8eb072.

--- src/components/create/create-header-content.tsx
+++ src/components/create/create-header-content.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-
-// might not need some of these things like the isStarted (but works right now so we will continue for now, clean up later?)
-// maybe make the header have the parameter, so we don't need a content header with something inside in another component, so we could have one instead of two
 export default function CreateRoomHeader({isStarted}: {isStarted?: boolean}) { 
+
+    //const exampleScramble = "Scramble Placeholder";
     return (
-        <h1 className="grow font-bold text-center text-2xl"> Create Room </h1>
+        <>
+            <h1 className="grow font-bold text-center text-2xl">
+                Create Room
+            </h1>
+        </>
     );
 }
