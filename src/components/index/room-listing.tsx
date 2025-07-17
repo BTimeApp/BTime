77c9cf74b6f63322c4513ca8f16f20223038cd17
Calix <calix.tang@gmail.com>
2025-07-16 19:42:08 -0700

    add user count to room listing

--- src/components/index/room-listing.tsx
+++ src/components/index/room-listing.tsx
@@ -9,7 +9,7 @@ import {
 } from "@/types/room";
 import JoinRoomButton from "@/components/index/join-room-button";
 import { Button } from "@/components/ui/button";
-import { RefreshCw } from 'lucide-react';
+import { RefreshCw } from "lucide-react";
 
 export default function RoomListing() {
   const [rooms, setRooms] = useState<Map<string, IRoom>>(
@@ -18,7 +18,7 @@ export default function RoomListing() {
 
   async function updateRooms() {
     const response = await fetch("/api/v0/rooms", {
-      method: "GET"
+      method: "GET",
     });
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -36,26 +36,26 @@ export default function RoomListing() {
   return (
     <div className="flex flex-col px-3">
       <div className="flex flex-row">
-        <h2 className="grow font-bold text-center text-xl">Rooms</h2>  
+        <h2 className="grow font-bold text-center text-xl">Rooms</h2>
         <Button variant="outline" size="sm" onClick={updateRooms}>
-          <RefreshCw/>
+          <RefreshCw />
         </Button>
-      </div>    
+      </div>
       <div className="px-1">
-        <div className="grid grid-cols-6 gap-3 py-1 text-left">
-          <div>Room Name</div>
+        <div className="grid grid-cols-8 gap-3 px-1 py-1 text-left shadow-sm rounded-sm">
+          <div className="col-span-3">Room Name</div>
+          <div>Users</div>
           <div>Event</div>
-          <div>Mode</div>
-          <div>Match Format</div>
-          <div>Set Format</div>
+          <div className="col-span-2">Format</div>
         </div>
         {rooms.size != 0 ? (
           [...rooms.entries()].map(([roomId, room]) => (
             <div
               key={roomId}
-              className="grid grid-cols-6 gap-3 py-1 text-left items-center"
+              className="grid grid-cols-8 gap-3 px-1 py-1 text-left items-center shadow-sm rounded-sm"
             >
-              <div>{room.roomName}</div>
+              <div className="col-span-3">{room.roomName}</div>
+              <div>{Object.keys(room.users).length}</div>
               <div className="flex flex-row">
                 <span
                   className={`cubing-icon ${ROOM_EVENT_ICON_SRC_MAP.get(
@@ -64,28 +64,26 @@ export default function RoomListing() {
                 ></span>
                 <div>{ROOM_EVENT_DISPLAY_NAME_MAP.get(room.roomEvent)}</div>
               </div>
-              <div>{room.roomFormat}</div>
-              {room.roomFormat === "RACING" ? (
-                <>
-                  <div>
-                    {MATCH_FORMAT_ABBREVIATION_MAP.get(room.matchFormat!)! +
-                      room.nSets!}
-                  </div>
-                  <div>
-                    {SET_FORMAT_ABBREVIATION_MAP.get(room.setFormat!)! +
-                      room.nSolves!}
-                  </div>
-                </>
-              ) : (
-                <></>
-              )}
+              <div className="col-span-2">
+                {room.roomFormat}{" "}
+                {room.roomFormat === "RACING" ? (
+                  "(" + MATCH_FORMAT_ABBREVIATION_MAP.get(room.matchFormat!)! +
+                  room.nSets! +
+                  " " +
+                  SET_FORMAT_ABBREVIATION_MAP.get(room.setFormat!)! +
+                  room.nSolves! + ")"
+                ) : (
+                  ""
+                )}
+              </div>
+
               {/* TODO: allow users to only need to type password for room once */}
               {/* {room.isPrivate ? (
                 <JoinPrivateRoomDialog roomId={room.id} roomName={room.roomName}></JoinPrivateRoomDialog>
               ) : (
                 <JoinRoomButton roomId={room.id}></JoinRoomButton>
               )} */}
-              <div className="col-start-6">
+              <div className="col-start-8">
                 <JoinRoomButton roomId={room.id}></JoinRoomButton>
               </div>
             </div>
