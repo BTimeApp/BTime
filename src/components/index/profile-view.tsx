a2c18fec5c6fb3574fc3a00a99d180b0ab007efa
Calix <calix.tang@gmail.com>
2025-07-20 00:49:15 -0700

    Revert "revert to avatar thumbnail, smaller profilepicture"
    This reverts commit ebae0d81462f41a8d6b7a4c28bbe3a86c8b40db8.

--- src/components/index/profile-view.tsx
+++ src/components/index/profile-view.tsx
@@ -37,8 +37,8 @@ export default function ProfileView() {
         <Image
           src={avatarURL ? avatarURL : "/images/C_logo.png"}
           alt="/images/C_logo.png"
-          width="120"
-          height="120"
+          width="200"
+          height="200"
           className="rounded-[50%] shadow-lg"
         />
       </div>
