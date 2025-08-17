6cf608b53c37eda8f135415b30c7bccb6bf1359e
Calix <calix.tang@gmail.com>
2025-08-16 23:09:38 -0700

    fix lint issues

--- src/components/room/room-settings-form.tsx
+++ src/components/room/room-settings-form.tsx
@@ -135,7 +135,7 @@ export default function RoomSettingsForm({
       setFormError(false);
       setFormErrorText("");
     }
-  }, [user, socket, socketConnected]);
+  }, [user, userLoading, socket, socketConnected]);
 
   const onSubmit = useCallback(
     (values: z.infer<typeof formSchema>) => {
@@ -164,7 +164,7 @@ export default function RoomSettingsForm({
         );
       }
     },
-    [socket, user]
+    [socket, user, createNewRoom, onCreateCallback, onUpdateCallback, roomId]
   );
 
   const onError = useCallback((errors: FieldErrors<FieldValues>) => {
