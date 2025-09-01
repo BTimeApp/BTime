d560c8546918b82b2b8cac7550c7fccad8c3ed81
Calix <calix.tang@gmail.com>
2025-08-31 23:03:16 -0700

    add playground page

---
+++ src/app/playground/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import Header from "@/components/common/header";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col h-screen w-full">
+      <Header>
+        <div className="flex flex-row">
+          <div className="flex-1">
+            <p className="text-3xl font-bold text-center">Playground</p>
+          </div>
+        </div>
+      </Header>
+      <div className="flex flex-col h-full w-full py-3">
+        <div className="text-center">
+          (Dev) Use this page to prototype quickly. 
+        </div>
+      </div>
+    </div>
+  );
+}
