87f68584d29a9b7667c9ea32a2454e45a9980bfb
Calix <calix.tang@gmail.com>
2025-09-10 23:04:57 -0700

    fix loading spinner dark mode

--- src/components/common/loading-spinner.tsx
+++ src/components/common/loading-spinner.tsx
@@ -3,7 +3,7 @@ import { LoaderCircle } from "lucide-react";
 export default function LoadingSpinner() {
   return (
     <div className="flex items-center justify-center h-[100%]">
-      <LoaderCircle className="animate-spin w-12 h-12 text-primary" />
+      <LoaderCircle className="animate-spin w-12 h-12 text-primary dark:text-primary-foreground" />
     </div>
   );
 }
