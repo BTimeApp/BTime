27e76b1596111bf1964b9c887aac648021e9745c
Calix <calix.tang@gmail.com>
2025-09-12 23:59:02 -0700

    enable tailwind conditional dark mode classes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -24,11 +24,11 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80 hover:text-secondary-accent/80 active:bg-secondary/60",
         ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+          "hover:text-accent-foreground hover:bg-accent/50",
         icon:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 px-0 hover:scale-105",
+          "hover:text-accent-foreground hover:bg-accent/50 px-0 hover:scale-105",
         danger:
-        "text-error font-bold bg-color-clear border-2 border-error hover:text-error  hover:scale-105",
+        "text-error font-bold bg-color-clear border-2 border-error hover:text-error hover:scale-105",
         reset:
           "bg-error text-error-foreground hover:bg-error/80 hover:text-error-foreground/80",
         link: "text-primary underline-offset-4 hover:underline",
