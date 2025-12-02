d85b343a92ab7179fcc2b4757d0dd1f62f82020b
Calix <calix.tang@gmail.com>
2025-12-02 10:54:59 -0800

    clean up comments

--- src/types/room-solve.ts
+++ src/types/room-solve.ts
@@ -1,16 +1,5 @@
 import { ISolve } from "@/types/solve";
 
-// export interface IRoomSolve {
-//   solve: ISolve;
-//   setIndex: number; //the set number that this solve belongs to
-//   solveIndex: number; //the solve number that this solve belongs to
-//   solveWinners: string[]; //user ID
-//   setWinners: string[]; //multiple winner is possible but unlikely
-//   matchWinners: string[];
-//   finished: boolean;
-// }
-
-
 export interface IRoomSolve {
   solve: ISolve;
   index: number; //the solve index WITHIN set
