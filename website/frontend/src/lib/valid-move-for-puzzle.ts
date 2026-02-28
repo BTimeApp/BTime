ed7eeedb44c397e128480229cd28c236f222f756
Calix <calix.tang@gmail.com>
2026-02-28 14:01:08 -0800

    update isValidMoveForPuzzle
    the moves and derivedMoves field on a kpuzzle's definition are not a complete source of truth for which moves are lega l on that specific puzzle. This commit reflects this by using a try catch-wrapped KPuzzle.moveToTransformation().
    
    In the future, we would like to switch to a non-try catch use of an official "is this move valid for this puzzle?" cubing.js api, but none exist yet.

--- website/frontend/src/lib/valid-move-for-puzzle.ts
+++ website/frontend/src/lib/valid-move-for-puzzle.ts
@@ -1,10 +1,32 @@
 import type { Move } from "cubing/alg";
 import type { KPuzzle } from "cubing/kpuzzle";
 
+/**
+ * helper function to check if a move is valid for a kpuzzle.
+ *
+ * Unfortunately, there are some (kpuzzle, move) combinations where the move is valid,
+ * but it isn't listed in the kpuzzle definition's moves or derivedMoves. For example,
+ * 3R, 2Rw are valid on 4x4 and up, but these moves aren't listed in moves or derivedMoves.
+ *
+ * The choices are either to manually account for these moves (which is too much effort)
+ * or to wrap moveToTransformation in a try/catch. For now, we will go with try/catch
+ *
+ * TODO: find an official "will this puzzle accept this move" API to use.
+ */
 export function isValidMoveForPuzzle(move: Move, kpuzzle: KPuzzle): boolean {
   const family = move.quantum.family;
-  return (
+
+  if (
     family in kpuzzle.definition.moves ||
     family in (kpuzzle.definition.derivedMoves ?? {})
-  );
+  ) {
+    return true;
+  }
+
+  try {
+    kpuzzle.moveToTransformation(move);
+    return true;
+  } catch {
+    return false;
+  }
 }
