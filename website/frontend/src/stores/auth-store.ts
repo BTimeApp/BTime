84054d2cb57b1145c959f07ed7df4abacef9ed96
Calix <calix.tang@gmail.com>
2026-04-21 22:19:48 -0700

    persist user in authstore

--- website/frontend/src/stores/auth-store.ts
+++ website/frontend/src/stores/auth-store.ts
@@ -1,6 +1,7 @@
 import type { IUser } from "@btime/types";
 
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 export type AuthState = {
   user: IUser | null;
@@ -8,8 +9,16 @@ export type AuthState = {
   setUser: (user: IUser | null) => void;
 };
 
-export const AuthStore = create<AuthState>((set) => ({
-  user: null,
-  hydrated: false,
-  setUser: (user) => set({ user, hydrated: true }),
-}));
+export const AuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      hydrated: false,
+      setUser: (user) => set({ user, hydrated: true }),
+    }),
+    {
+      name: "auth-storage",
+      partialize: (state) => ({ user: state.user }),
+    },
+  ),
+);
